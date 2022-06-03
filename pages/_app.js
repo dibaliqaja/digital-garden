24309bc6476c4e14cf01eb20c23cdd12938f79eb
dibaliqaja <dibaliqaja@gmail.com>
2022-06-03 15:57:15 +0700

    feat: add nprogress for load page

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,14 @@ import Head from 'next/head'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
